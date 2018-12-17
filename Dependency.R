deb36b8d2f95f6109821641b335be75027468cc4
akshaynarasimhan <43821896+akshaynarasimhan@users.noreply.github.com>
2018-12-17 22:40:40 +0530

    Add files via upload

---
+++ Dependency.R
@@ -0,0 +1,13 @@
+#dependency RMD
+if(!require(udpipe)){install.packages("udpipe")}
+if(!require(textrank)){install.packages("textrank")}
+if(!require(lattice)){install.packages("lattice")}
+if(!require(igraph)){install.packages("igraph")}
+if(!require(ggraph)){install.packages("ggraph")}
+if(!require(wordcloud))){install.packages("wordcloud")}
+library(udpipe)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggraph)
+library(wordcloud)
